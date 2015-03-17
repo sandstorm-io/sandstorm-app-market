6aedc796ca18cad40e8df8ba1a1f4bf8e40bc2f1
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2015-03-17 18:22:49 +0000

    css basics for layout page

---
+++ app/client/templates/components/app-item.js
@@ -0,0 +1,10 @@
+Template.appItem.helpers({
+
+  priceFormat: function(price) {
+
+    // TODO: update function to pretty print price
+    return 'FREE';
+
+  }
+
+});
---
+++ app/client/templates/components/topbar.js
@@ -0,0 +1,45 @@
+var genres = [
+  {
+    name: 'all',
+    route: '/',
+    active: true
+  },
+  {
+    name: 'popular',
+    route: '/'
+  },
+  {
+    name: 'productivity',
+    route: '/'
+  },
+  {
+    name: 'publishing',
+    route: '/'
+  },
+  {
+    name: 'games',
+    route: '/'
+  },
+  {
+    name: 'email',
+    route: '/'
+  },
+  {
+    name: 'science',
+    route: '/'
+  },
+  {
+    name: 'media',
+    route: '/'
+  },
+  {
+    name: 'social',
+    route: '/'
+  }
+];
+
+Template.Topbar.helpers({
+  genres: function() {
+    return genres;
+  }
+});
