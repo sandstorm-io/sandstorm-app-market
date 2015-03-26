4074dc08d0f4a21da95eee728499f5bcf44e371b
Richard Silverton <silverton.richard@googlemail.com>
2015-03-26 11:07:40 +0000

    added browser policy and fixed scaling on widescreen

--- app/client/templates/home/home.js
+++ app/client/templates/home/home.js
@@ -30,12 +30,6 @@ Template.Home.onCreated(function () {
   this.subscribe('categories');
 });
 
-Template.Home.rendered = function () {
-};
-
-Template.Home.destroyed = function () {
-};
-
 Template.genreTable.onCreated(function() {
   this.subscribe('apps by genre', this.data);
 });
---
+++ app/server/lib/browser-policy.js
@@ -0,0 +1,5 @@
+BrowserPolicy.content.allowImageOrigin("*");
+BrowserPolicy.content.allowStyleOrigin('http://fonts.googleapis.com');
+BrowserPolicy.content.allowFontOrigin('http://fonts.gstatic.com');
+BrowserPolicy.content.allowInlineStyles();
+BrowserPolicy.content.allowFontDataUrl();
