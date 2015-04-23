8fc17fc29111d4d6f269fae755fde2872a79cf2d
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2015-04-23 22:13:08 +0100

    fixed github login

--- app/server/accounts.js
+++ app/server/accounts.js
@@ -1,7 +1,8 @@
 Accounts.onCreateUser(function(options, user) {
   if (options.profile) user.profile = options.profile;
   if (!user.username) {
-    if (user.services &&user.services.google) user.username = user.services.google.email;
+    if (user.services && user.services.google) user.username = user.services.google.email;
+    else if (user.services && user.services.github) user.username = user.services.github.username;
     else if (user.emails) user.username = user.emails[0].address;
   }
   return user;
