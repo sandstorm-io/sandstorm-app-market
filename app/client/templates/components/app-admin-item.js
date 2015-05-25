b0b0f61792cd86354076d13f600866085d2b649d
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2015-05-25 19:04:49 +0100

    allow user to enter notes on admin console page. Fixes #118

--- app/client/templates/components/app-admin-item.js
+++ app/client/templates/components/app-admin-item.js
@@ -1,3 +1,7 @@
+function logErr(err, res) {
+  if (err) console.log(err);
+}
+
 Template.appAdminItem.onCreated(function() {
 
   this.filters = this.get('filters');
@@ -23,25 +27,37 @@ Template.appAdminItem.helpers({
 
 Template.appAdminItem.events({
 
+  'keyup [data-field="admin-notes"]': function(evt, tmp) {
+    if (evt.keyCode === 13)
+      if (!evt.shiftKey) {
+        var note = tmp.$('[data-field="admin-notes"]').val();
+        if (!s.isBlank(note)) Meteor.call('apps/addNote', this._id, note, logErr);
+        tmp.$('[data-field="admin-notes"]').val('');
+      }
+  },
   'click [data-action="approve"]': function(evt, tmp) {
-    Meteor.call('apps/approve', this._id, function(err, res) {
-      if (err) console.log(err);
-    });
+    var note = tmp.$('[data-field="admin-notes"]').val();
+    if (!s.isBlank(note)) Meteor.call('apps/addNote', this._id, note, logErr);
+    Meteor.call('apps/approve', this._id, logErr);
+    tmp.$('[data-field="admin-notes"]').val('');
   },
   'click [data-action="request-revision"]': function(evt, tmp) {
-    Meteor.call('apps/request-revision', this._id, function(err, res) {
-      if (err) console.log(err);
-    });
+    var note = tmp.$('[data-field="admin-notes"]').val();
+    if (!s.isBlank(note)) Meteor.call('apps/addNote', this._id, note, logErr);
+    Meteor.call('apps/request-revision', this._id, logErr);
+    tmp.$('[data-field="admin-notes"]').val('');
   },
   'click [data-action="flag"]': function(evt, tmp) {
-    Meteor.call('apps/flag', this._id, function(err, res) {
-      if (err) console.log(err);
-    });
+    var note = tmp.$('[data-field="admin-notes"]').val();
+    if (!s.isBlank(note)) Meteor.call('apps/addNote', this._id, note, logErr);
+    Meteor.call('apps/flag', this._id, logErr);
+    tmp.$('[data-field="admin-notes"]').val('');
   },
   'click [data-action="reject"]': function(evt, tmp) {
-    Meteor.call('apps/reject', this._id, function(err, res) {
-      if (err) console.log(err);
-    });
+    var note = tmp.$('[data-field="admin-notes"]').val();
+    if (!s.isBlank(note)) Meteor.call('apps/addNote', this._id, note, logErr);
+    Meteor.call('apps/reject', this._id, logErr);
+    tmp.$('[data-field="admin-notes"]').val('');
   }
 
 });
