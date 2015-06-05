d58aa59b16fbafdf7f980229013b9ab46858fce1
Richard Silverton <silverton.richard@googlemail.com>
2015-06-05 14:50:00 +0100

    rigged up test collection

--- app/lib/collections/spks.js
+++ app/lib/collections/spks.js
@@ -79,3 +79,70 @@ if (Meteor.isServer) {
   });
 
 }
+
+spkGCS = new FS.Store.S3('spkGCS', {
+  region: Meteor.settings.public.AWSRegion, //optional in most cases
+  projectId: Meteor.isServer && Meteor.settings.GCSProjectId, //required if environment variables are not set
+  keyFilename: Meteor.isServer && Meteor.settings.GCSkKeyFilename, //required if environment variables are not set
+  bucket: Meteor.settings.public.spkBucket, //required
+  ACL: 'public-read', //optional, default is 'private', but you can allow public or secure access routed through your app URL
+});
+
+
+TestColl = new FS.Collection('testcoll', {
+  stores: [
+    new FS.Store.FileSystem('testFS', {path: 'uploads/test'}),
+    spkGCS
+    ],
+  filter: {
+    maxSize: 500 * 1024 * 1024, // in bytes
+    allow: {
+      extensions: ['spk']
+    },
+    onInvalid: function (message) {
+      console.log(message);
+    }
+  }
+});
+
+TestColl.allow({
+  insert: function(userId, doc) {
+    return true;
+  },
+  update: function(userId, doc, fieldNames, modifier) {
+    return true;
+  },
+  remove: function(userId, doc) {
+    return true;
+  },
+  download:function(){
+    return true;
+  }
+});
+
+if (Meteor.isServer) {
+
+  Meteor.publish('test', function(fileId) {
+    return TestColl.find({$or: [{_id: fileId}, {'meta.appId': fileId}]});
+  });
+
+  TestColl.find().observeChanges({
+    changed: function(id, fields) {
+      if (fields.copies && fields.copies.spkFS) {
+        if (TestColl.findOne(id).meta) return;
+        try {
+          var packageMeta = App.spkVerify('uploads/spks/' + fields.copies.spkFS.key),
+              existing = Apps.findOne({'versions.packageId': packageMeta.packageId}),
+              latest = TestColl.findOne({'meta.appId': packageMeta.appId}, {sort: {'meta.version': -1}});
+          if (existing) TestColl.update(id, {$set: {error: 'DUPLICATE_SPK'}});
+          if (latest && latest.meta.version > packageMeta.version) TestColl.update(id, {$set: {error: 'NON_LATEST_VERSION'}});
+          TestColl.update(id, {$set: {meta: packageMeta}});
+        } catch(e) {
+          console.log(e);
+          TestColl.update(id, {$set: {error: 'BAD_SPK'}});
+        }
+      }
+    }
+  });
+
+}
