a61fb96ee21279a7370e4a42c2456fb1beae563b
Asheesh Laroia <asheesh@sandstorm.io>
2016-07-29 12:07:18 -0700

    Use lunrjs for client-side search
    Close #12

--- app/client/templates/search/search.js
+++ app/client/templates/search/search.js
@@ -1,16 +1,37 @@
+var createEmptyLunrIndex = function() {
+  var index = lunr(function() {
+    this.field('text');
+    this.ref('_id');
+  });
+  return index;
+};
+
 Template.Search.helpers({
   searchQuery: function() {
     return FlowRouter.getQueryParam('term');
   },
+  stillWaitingOnApps: function() {
+    return Apps.find({}).count() == 0;
+  },
   appList: function() {
-
+    // For now, we do no caching.
+    var index, documentsToIndex, searchResults;
     var term = FlowRouter.getQueryParam('term');
-    return term && Apps.find({$or: [
-      {name: {$regex: term, $options: 'i'}},
-      {shortDescription: {$regex: term, $options: 'i'}},
-      {description: {$regex: term, $options: 'i'}}
-    ]}, {sort: {name: -1}}).fetch();
-
+    var results = [];
+    if (! term) {
+      return;
+    }
+    index = createEmptyLunrIndex();
+    documentsToIndex = _.map(Apps.find({}).fetch(), function(thing) {
+      return {_id: thing._id,
+              text: ((thing.name || "") + " " +
+                     (thing.shortDescription || "") + " " +
+                     (thing.description || ""))}
+    });
+    _.each(documentsToIndex, function(doc) { index.add(doc); });
+    searchResults = index.search(term);
+    var resultAppIds = _.map(searchResults, function(result) { return result.ref; });
+    return Apps.find({_id: {$in: resultAppIds}},
+                     {sort: {name: -1}}).fetch();
   }
-
 });
